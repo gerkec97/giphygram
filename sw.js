6a0b60d6a593fe1194c3a493695675d08f44802f
Gerrit Keck <gerrit.keck@globalgaming.com>
2021-01-05 17:34:12 +0100

    Caching added for giphy images

--- sw.js
+++ sw.js
@@ -31,12 +31,12 @@ self.addEventListener('activate', e => {
 })
 
 // Cache with network fallback strategy
-const staticCache = (req) => {
+const staticCache = (req, cacheName = `static-${version}`) => {
   return caches.match(req).then(cachedResponse => {
     if (cachedResponse) return cachedResponse
 
     return fetch(req).then(networkResponse => {
-      caches.open(`static-${version}`).then(cache => {
+      caches.open(cacheName).then(cache => {
         cache.put(req, networkResponse.clone())
         return networkResponse
       })
@@ -44,9 +44,26 @@ const staticCache = (req) => {
   })
 }
 
+// Network first local fallback strategy
+const fallbackCache = (req) => {
+  return fetch(req).then(networkResponse => {
+
+    if (!networkResponse.ok) throw 'Fetch Error'
+
+    caches.open(`static-${version}`).then(cache => cache.put(req, networkResponse))
+
+    return networkResponse.clone()
+  }).catch(err => caches.match(req))
+}
+
 self.addEventListener('fetch', e => {
   // App Shell resources only
   if (e.request.url.match(location.origin)) {
     e.respondWith(staticCache(e.request))
+  } else if (e.request.url.match('https://api.giphy.com/v1/gifs/trending')) {
+    e.respondWith(fallbackCache(e.request))
+  } else if (e.request.url.match('giphy.com/media')) {
+    e.respondWith(staticCache(e.request, 'giphy-media'))
   }
+
 })
